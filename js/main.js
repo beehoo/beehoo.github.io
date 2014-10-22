9c436a976fbb203dc0b7fdd4870fb6f2b659a174
beehoo <824624059@qq.com>
2014-10-22 23:49:31 +0800

    Jquery版本降回1.8，不对IE9以下版本做兼容。优化部分体验

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(function(){
-	
+
+	//首页文章显示截取字数
 	$(".content article").each(function(){
 		var _this = $(this),
 		_txt = _this.html().replace(/<[^>]+>/g,""),
@@ -10,6 +11,18 @@ $(function(){
 		};
 	});
 
+	//内容区最小高度
 	$(".wp").css("min-height",$(window).height() - 157);
-	
+
+	//博主头像切换
+	$(".blogger_img img").dblclick(function(){
+		var _this = $(this);
+		_this.addClass("none");
+		_this.siblings("img.none").removeClass("none");
+	});
+
+	//判断浏览器
+	if($.browser.msie && $.browser.version < 9) {
+		alert("您好。本博客不对IE9以下的版本做兼容。推荐您使用Chrome浏览器或使用更高版本的IE。");
+	};
 });
